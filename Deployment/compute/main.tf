7b2484d2037f4ac0ae67c5b57278626fcd1e623c
Navinder Gill <126389118+navigill7@users.noreply.github.com>
2025-07-02 16:34:10 +0530

    Update main.tf

--- Deployment/compute/main.tf
+++ Deployment/compute/main.tf
@@ -5,42 +5,34 @@ resource "aws_instance" "minikube_instance" {
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
 
-  user_data = <<-EOF
-              #!/bin/bash
-              set -e
+  user_data = <<EOF
+#!/bin/bash
 
-              # Update and install dependencies
-              apt-get update -y
-              apt-get upgrade -y
-              apt-get install -y curl wget apt-transport-https ca-certificates gnupg lsb-release socat conntrack
+# Update system
+apt update -y && apt upgrade -y
 
-              # Install Docker
-              apt-get install -y docker.io
-              systemctl enable docker
-              systemctl start docker
+# Install Docker
+apt install -y docker.io
+systemctl enable docker
+systemctl start docker
+usermod -aG docker ubuntu
 
-              # Add user to docker group
-              usermod -aG docker ubuntu
+# Install conntrack (required by Minikube)
+apt install -y conntrack
 
-              # Install kubectl
-              curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-              install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -sL https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin/
 
-              # Install Minikube
-              curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-              dpkg -i minikube_latest_amd64.deb
+# Install Minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+install minikube /usr/local/bin/
 
-              # Start Minikube with Docker driver
-              export CHANGE_MINIKUBE_NONE_USER=true
-              minikube start --driver=docker --force
-
-              # Enable addons
-              minikube addons enable default-storageclass
-              minikube addons enable storage-provisioner
-
-              # Set minikube as default context
-              kubectl config use-context minikube
-              EOF
+# Start Minikube using Docker driver
+sudo -u ubuntu minikube start --driver=docker
+EOF
 
   tags = {
     Name = "MiniKubeInstance"
